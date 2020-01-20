4119a12056a2fb5f388c06dad81c20fefb881997
DeeJayTC <DeeJay@tcdev.de>
2020-01-20 23:09:55 +0100

    basic empty test client

---
+++ Source/.Net/Templates/InbuiltTemplates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiTransformer.Templates
+{
+  public static class Inbuilt
+  {
+
+    public enum Templates
+    {
+
+    }
+
+
+  }
+}
